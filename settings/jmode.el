ab7f75eaea98fece1a8bae13e63f9f85c0048f96
cafama <matteo.cafasso@f-secure.com>
2015-10-28 18:06:39 +0200

    fix java mode setting

--- settings/jmode.el
+++ settings/jmode.el
@@ -1,9 +1,8 @@
-;; C/C++ Mode
+;; Java Mode
 
 (require 'jdee)
 (require 'column-marker)
 
-(add-hook 'java-mode-hook 'jdee-mode)
 (add-hook 'java-mode-hook (lambda () (column-marker-1 80)))
 
 (provide 'jmode)
