c9f2bc2775fceb01f00f027697c71d8bfbae78e7
noxdafox <noxdafox@gmail.com>
2015-11-07 18:07:31 +0200

    fix company color when emacs is used without X

--- settings/completion.el
+++ settings/completion.el
@@ -10,13 +10,19 @@
 (define-key company-active-map (kbd "<tab>") #'company-complete)
 ;; CompAny color face setting
 (require 'color)
-(let ((bg (face-attribute 'default :background)))
-  (when bg (not "unspecified-bg")
-          (custom-set-faces
-           `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-           `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-           `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-           `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-           `(company-tooltip-common ((t (:inherit font-lock-constant-face)))))))
+(defun background-color ()
+  "Return background color."
+  (let ((background (face-attribute 'default :background)))
+  (if (equal background "unspecified-bg")
+      "#000000"
+    background)))
+
+(let ((bg (background-color)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
 
 (provide 'completion)
