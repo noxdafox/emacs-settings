92e3a831ac12a9f6d94528c64154db0ccb045b20
Matteo Cafasso <noxdafox@gmail.com>
2016-11-25 00:15:39 +0200

    ggtags mode improvements
    Hooked to prog-mode.
    
    Update tags when saving buffers.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- settings/cmode.el
+++ settings/cmode.el
@@ -1,14 +1,9 @@
 ;; C/C++ Mode
 
-(require 'ggtags)
-
 (custom-set-variables
  '(c-basic-offset 4)
  '(c-indent-level 4)
  '(c-file-style "K&R"))
-
-(add-hook 'c-mode-hook 'ggtags-mode)
-(add-hook 'c++-mode-hook 'ggtags-mode)
 
 ;; disable electric comma
 (eval-after-load "cc-mode"
--- settings/pymode.el
+++ settings/pymode.el
@@ -1,13 +1,11 @@
 ;; Python Mode
 
-(require 'ggtags)
 (require 'flycheck)
 
 ;; disable ycmd-flycheck for python
 (add-hook 'python-mode-hook
           (lambda () (add-to-list 'flycheck-disabled-checkers 'ycmd)))
 
-(add-hook 'python-mode-hook 'ggtags-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
 (setq flycheck-python-pylint-executable "pylint3")
 
