45820cbe234e243ec01e2f316563a34b01ac3382
Matteo Cafasso <noxdafox@gmail.com>
2016-11-28 19:16:57 +0200

    completion: decrease code completion delay
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- settings/completion.el
+++ settings/completion.el
@@ -8,6 +8,8 @@
 (require 'company)
 (global-company-mode t)
 
+(set 'company-idle-delay 0.1)
+
 ;; ycmd company backend settings
 (set-variable 'ycmd-server-command '("python" "/usr/lib/ycmd/ycmd"))
 (define-key company-active-map (kbd "<tab>") #'company-complete)
