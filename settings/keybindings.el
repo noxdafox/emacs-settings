e8b192c1b472fc7cffe58407f20ca8b2e7aaa2fb
Matteo Cafasso <noxdafox@gmail.com>
2017-02-07 15:18:05 +0200

    keybindings: improved region shifting logic
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- settings/keybindings.el
+++ settings/keybindings.el
@@ -96,9 +96,6 @@
   (let ((mark (mark)))
     (save-excursion
       (indent-rigidly (region-beginning) (region-end) distance)
-      (push-mark mark t t)
-      ;; Tell the command loop not to deactivate the mark
-      ;; for transient mark mode
       (setq deactivate-mark nil))))
 
 (defun comment-or-uncomment ()
