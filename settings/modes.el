7d77b59c85582260f542466644b9daf73d77e562
noxdafox <noxdafox@gmail.com>
2018-01-16 17:05:19 +0200

    python-mode: remove simple-prompt from IPython shell
    IPython > 5.4 supports old readline based interface via rlipython
    project.
    
    Signed-off-by: noxdafox <noxdafox@gmail.com>

--- settings/modes.el
+++ settings/modes.el
@@ -30,7 +30,7 @@
 
 ; ipython interpreter
 (setq python-shell-interpreter "ipython3"
-      python-shell-interpreter-args "--simple-prompt -i")
+      python-shell-interpreter-args "-i")
 
 ;; Comint mode
 (custom-set-variables
