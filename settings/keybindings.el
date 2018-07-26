103edfbaf26bbd42031d704fc3b9e215dea2508f
noxdafox <noxdafox@gmail.com>
2018-07-26 15:53:45 +0300

    keybindings: change keymapping for buffer revert command
    It was conflicting with Emacs region commands.
    
    Signed-off-by: noxdafox <noxdafox@gmail.com>

--- settings/keybindings.el
+++ settings/keybindings.el
@@ -58,7 +58,7 @@
 (bind-key* "<f8>" 'sr-speedbar-toggle)
 
 ;; force the reload of a buffer
-(bind-key* "C-x r" 'force-revert-buffer)
+(bind-key* "C-x R" 'force-revert-buffer)
 
 ;; special characters
 (bind-key* (kbd "C-a") 'special-character)
