898747434bd1d67a2ff715df6b23d90a1adaf3ac
noxdafox <noxdafox@gmail.com>
2018-12-27 17:16:54 +0200

    development: switch from `ycmd` to `lsp`, adopt `use-package`
    Signed-off-by: noxdafox <noxdafox@gmail.com>

--- settings/development.el
+++ settings/development.el
@@ -1,41 +1,40 @@
 ;; Development specific configuration
 
-;; auto completion through ycmd-company
-(require 'ycmd)
-(add-hook 'after-init-hook #'global-ycmd-mode)
-(set-variable 'ycmd-python-binary-path "/usr/bin/python3")
-(set-variable 'ycmd-server-command
-              '("python3" "/home/noxdafox/development/ycmd/ycmd"))
-(set-variable 'ycmd-global-config
-              "/home/noxdafox/.emacs.d/ycmd_global_config.py")
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t)
+  (setq company-idle-delay 0.1))
 
-(require 'company-ycmd)
-(company-ycmd-setup)
+(use-package flycheck
+  :ensure t
+  :hook (after-init-hook . global-flycheck-mode))
 
-(require 'company)
-(global-company-mode t)
-(set 'company-idle-delay 0.1)
+;; Language Server Protocol
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :hook (python-mode-hook . lsp)
+  :config
+  (setq lsp-hover-enabled t)
+  (setq lsp-auto-guess-root t)
+  (setq lsp-signature-enabled t)
+  (setq lsp-prefer-flymake nil))
 
-(require 'company-posframe)
-(company-posframe-mode 1)
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+;; Code navigation
+(use-package ggtags
+  :hook ((prog-mode-hook . ggtags-mode)
+         (after-save-hook . gtags-update-hook))
 
 ;; Enable line-number-mode when developing
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
-;; flycheck syntax checker
-(require 'flycheck)
-(require 'flycheck-ycmd)
-(flycheck-ycmd-setup)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; code documentation
-(require 'ycmd-eldoc)
-(add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup)
-
-;; code navigation
-(require 'ggtags)
-(add-hook 'prog-mode-hook 'ggtags-mode)
-(add-hook 'after-save-hook #'gtags-update-hook)
 
 (defun gtags-root-dir ()
   "Return GTAGS root directory or nil if doesn't exist."
