6470770868c14d594877219e9d97ce4d8147b4f3
Matteo Cafasso <noxdafox@gmail.com>
2019-09-17 09:51:12 +0300

    modes: fix progress bar for programs such as apt
    https://oremacs.com/2019/03/24/shell-apt/
    
    Thanks to @abo-abo for this tip!
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- settings/modes.el
+++ settings/modes.el
@@ -44,10 +44,35 @@
  '(comint-buffer-maximum-size 8192)     ; maximum buffer size in lines
  '(comint-prompt-read-only t))          ; set comint prompt read-only
 
-(defun comint-preoutput-turn-buffer-read-only (text)
-  (propertize text 'read-only t))
 (add-hook 'comint-preoutput-filter-functions
           'comint-preoutput-turn-buffer-read-only)
+
+(defun comint-preoutput-turn-buffer-read-only (text)
+  (propertize text 'read-only t))
+
+; Fix progress bar for programs such as apt
+(advice-add
+ 'ansi-color-apply-on-region
+ :before 'ora-ansi-color-apply-on-region)
+
+(defun ora-ansi-color-apply-on-region (begin end)
+  "Display progress bar in the mode line."
+  (let ((end-marker (copy-marker end)) mb)
+    (save-excursion
+      (goto-char (copy-marker begin))
+      (while (re-search-forward "\0337" end-marker t)
+        (setq mb (match-beginning 0))
+        (when (re-search-forward "\0338" end-marker t)
+          (ora-apt-progress-message
+           (substring-no-properties
+            (delete-and-extract-region mb (point))
+            2 -2)))))))
+
+(defun ora-apt-progress-message (progress)
+  (message
+   (replace-regexp-in-string
+    "%" "%%"
+    (ansi-color-apply progress))))
 
 ; do not override prompt colors
 (set-face-attribute 'comint-highlight-prompt nil :inherit nil)
